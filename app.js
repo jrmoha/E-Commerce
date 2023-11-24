14d26b75e9eb17b67c5ddd5ce4e9b676af7cce54
Mustafa <mostafa.dash34@gmail.com>
2023-11-24 07:18:37 +0200

    modified:   app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
+import 'dotenv/config';
 import userAuthRoute from './routes/userAuthRoute.js';
 import session from 'express-session';
 import favRoute from './routes/favoritesRoute.js';
@@ -10,21 +9,19 @@ import productsRoute from './routes/productsRoute.js';
 import usersRoute from './routes/usersRoute.js';
 
 
-dotenv.config();
 const app = express();
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(session({
-    secret: "This Is My Stupid Application",
+    secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
     cookie: {},
 }));
-app.use((err, req, res, next) => {
-    res.status(500).send(err.Stack);
-});
+
 
 app.use('/', userAuthRoute);
 app.use('/', favRoute);
@@ -33,4 +30,8 @@ app.use('/', trendingRoute);
 app.use('/', productsRoute);
 app.use('/', usersRoute);
 
-app.listen(process.env.PORT, '127.0.0.1');
+app.use((err, _req, res, _next) => {
+    res.status(500).send(err.Stack);
+});
+
+app.listen(process.env.PORT);
